375ad4b72a64fc1f4de545c560e2566eebadb990
bvoelker <bvoelker@bvoelker.speedport.ip>
2021-10-06 11:50:14 +0200

    add plotting functionality and insightful plots

---
+++ plot_times.py
@@ -0,0 +1,11 @@
+import argparse
+from pathlib import Path
+from flow.utils import Plotter
+
+parser = argparse.ArgumentParser(description='Argument parser for summary plot creation.')
+parser.add_argument('-c', '--conf', type=str, required=True, help='Path to the summary plot config file.')
+
+args = parser.parse_args()
+
+plot_cnf = Path(args.conf)
+Plotter.plot_times(plot_cnf)
