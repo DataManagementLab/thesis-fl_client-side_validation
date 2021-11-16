c74ffdcb38aa0dc82aed20a93446239baec32454
voelker.benedikt@yahoo.de <voelker.benedikt@yahoo.de>
2021-11-16 09:07:13 +0100

    Add device settings to training functions

--- flow/training/untargeted_attack.py
+++ flow/training/untargeted_attack.py
@@ -2,16 +2,16 @@ import torch
 from flow.utils import Logger
 from flow.utils.model_poisoning import gradient_noise
 
-def untargeted_attack(model, optimizer, loss_fn, data, target, epoch, batch, logger: Logger, frequency=0.1, scale=1/5):
+def untargeted_attack(model, optimizer, loss_fn, data, target, epoch, batch, device, logger: Logger, frequency=0.1, scale=1/5):
     assert 0 < frequency < 1
     assert 0 < scale
     optimizer.zero_grad()
-    output = model(data)
-    loss = loss_fn(output, target)
+    output = model(data.to(device))
+    loss = loss_fn(output, target.to(device))
     loss.backward()
     for l, weight in model.named_parameters():
         if torch.rand(1).item() <= frequency:
-            noise = gradient_noise(weight.shape, scale=scale)
+            noise = gradient_noise(weight.shape, scale=scale, device=device)
             weight.grad += noise
             logger.log_attack_application(epoch, batch, l, "GRADIENT_NOISE")
             # print(f'\tWeight: {l}')
