a70240ac06f8983fcf74ccd08de910028a868435
benvoe <voelker.benedikt@yahoo.de>
2021-11-16 09:44:08 +0100

    play around with tracemalloc

--- flow/validation/core.py
+++ flow/validation/core.py
@@ -1,4 +1,4 @@
-import torch
+import torch, tracemalloc
 
 def validate_buffer(buffer, validation_fn, model_builder, optimizer_builder, loss_fn_builder, time_tracker, logger):
     torch.device('cpu')
@@ -10,6 +10,7 @@ def validate_buffer(buffer, validation_fn, model_builder, optimizer_builder, los
     optimizer = optimizer_builder(model.parameters())
     loss_fn = loss_fn_builder()
     
+    tracemalloc.start()
     for index, vset in buffer.items():
 
         model.load_state_dict(vset.get_model_start())
@@ -28,5 +29,7 @@ def validate_buffer(buffer, validation_fn, model_builder, optimizer_builder, los
             validation_set=vset
         )
         time_tracker.stop('raw_time_validation')
+    print('mem after:', tracemalloc.get_traced_memory())
+    tracemalloc.stop()
     
     time_tracker.stop('total_time_validation')
