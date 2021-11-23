c57bd5238d317a9c549a9a0bf3297264f9071ff0
benvoe <voelker.benedikt@yahoo.de>
2021-11-23 12:29:09 +0100

    Add buffer size experiment to thesis experiments

--- thesis_experiments.sh
+++ thesis_experiments.sh
@@ -2,41 +2,64 @@
 
 N=5
 
-EXPERIMENT_1=false
-EXPERIMENT_2=true
+EXPERIMENT_METHOD_SCALABILITY=false
+EXPERIMENT_GUARANTEE_SCALABILITY=false
 
-if $EXPERIMENT_1; then
-echo "===== TIME EXPERIMENTS 1 ====="
-echo "===== TIME EXPERIMENTS | Freivald ====="
+EXPERIMENT_BUFFER_SIZE=true
+
+if $EXPERIMENT_METHOD_SCALABILITY; then
+echo "===== TIME EXPERIMENT_METHOD_SCALABILITY ====="
+echo "===== TIME EXPERIMENT_METHOD_SCALABILITY | Freivald ====="
 python run_exp.py -r $N -c experiments/time/freivald/l2_512_sync_cpu_bt64_bf32_n1.yml
 python run_exp.py -r $N -c experiments/time/freivald/l4_512_sync_cpu_bt64_bf32_n1.yml
 python run_exp.py -r $N -c experiments/time/freivald/l6_512_sync_cpu_bt64_bf32_n1.yml
 python run_exp.py -r $N -c experiments/time/freivald/l8_512_sync_cpu_bt64_bf32_n1.yml
 
-echo "===== TIME EXPERIMENTS | MatMul ====="
+echo "===== TIME EXPERIMENT_METHOD_SCALABILITY | MatMul ====="
 python run_exp.py -r $N -c experiments/time/matmul/l2_512_sync_cpu_bt64_bf32_n1.yml
 python run_exp.py -r $N -c experiments/time/matmul/l4_512_sync_cpu_bt64_bf32_n1.yml
 python run_exp.py -r $N -c experiments/time/matmul/l6_512_sync_cpu_bt64_bf32_n1.yml
 python run_exp.py -r $N -c experiments/time/matmul/l8_512_sync_cpu_bt64_bf32_n1.yml
 
-echo "===== TIME EXPERIMENTS | Retrain ====="
+echo "===== TIME EXPERIMENT_METHOD_SCALABILITY | Retrain ====="
 python run_exp.py -r $N -c experiments/time/retrain/l2_512_sync_cpu_bt64_bf32_n1.yml
 python run_exp.py -r $N -c experiments/time/retrain/l4_512_sync_cpu_bt64_bf32_n1.yml
 python run_exp.py -r $N -c experiments/time/retrain/l6_512_sync_cpu_bt64_bf32_n1.yml
 python run_exp.py -r $N -c experiments/time/retrain/l8_512_sync_cpu_bt64_bf32_n1.yml
 
-echo "===== TIME EXPERIMENTS | SubMul ====="
+echo "===== TIME EXPERIMENT_METHOD_SCALABILITY | SubMul ====="
 python run_exp.py -r $N -c experiments/time/submul/l2_512_sync_cpu_bt64_bf32_n1.yml
 python run_exp.py -r $N -c experiments/time/submul/l4_512_sync_cpu_bt64_bf32_n1.yml
 python run_exp.py -r $N -c experiments/time/submul/l6_512_sync_cpu_bt64_bf32_n1.yml
 python run_exp.py -r $N -c experiments/time/submul/l8_512_sync_cpu_bt64_bf32_n1.yml
 fi
 
-if $EXPERIMENT_2; then
-echo "===== TIME EXPERIMENTS 2 ====="
-echo "===== TIME EXPERIMENTS | Freivald ====="
+if $EXPERIMENT_GUARANTEE_SCALABILITY; then
+echo "===== TIME EXPERIMENT_GUARANTEE_SCALABILITY ====="
+echo "===== TIME EXPERIMENT_GUARANTEE_SCALABILITY | Freivald ====="
 python run_exp.py -r $N -c experiments/time/freivald/l2_512_sync_cpu_bt64_bf32_q99.yml
 python run_exp.py -r $N -c experiments/time/freivald/l4_512_sync_cpu_bt64_bf32_q99.yml
 python run_exp.py -r $N -c experiments/time/freivald/l6_512_sync_cpu_bt64_bf32_q99.yml
 python run_exp.py -r $N -c experiments/time/freivald/l8_512_sync_cpu_bt64_bf32_q99.yml
+fi
+
+if $EXPERIMENT_BUFFER_SIZE; then
+echo "===== MEMORY EXPERIMENT_BUFFER_SIZE ====="
+echo "===== MEMORY EXPERIMENT_BUFFER_SIZE | Freivald ====="
+python run_exp.py -r $N -c experiments/memory/freivald/l2_512_async_cpu_bt64_bf8_n1.yml
+python run_exp.py -r $N -c experiments/memory/freivald/l2_512_async_cpu_bt64_bf16_n1.yml
+python run_exp.py -r $N -c experiments/memory/freivald/l2_512_async_cpu_bt64_bf32_n1.yml
+python run_exp.py -r $N -c experiments/memory/freivald/l2_512_async_cpu_bt64_bf64_n1.yml
+
+echo "===== MEMORY EXPERIMENT_BUFFER_SIZE | MatMul ====="
+python run_exp.py -r $N -c experiments/memory/matmul/l2_512_async_cpu_bt64_bf8_n1.yml
+python run_exp.py -r $N -c experiments/memory/matmul/l2_512_async_cpu_bt64_bf16_n1.yml
+python run_exp.py -r $N -c experiments/memory/matmul/l2_512_async_cpu_bt64_bf32_n1.yml
+python run_exp.py -r $N -c experiments/memory/matmul/l2_512_async_cpu_bt64_bf64_n1.yml
+
+echo "===== MEMORY EXPERIMENT_BUFFER_SIZE | SubMul ====="
+python run_exp.py -r $N -c experiments/memory/submul/l2_512_async_cpu_bt64_bf8_n1.yml
+python run_exp.py -r $N -c experiments/memory/submul/l2_512_async_cpu_bt64_bf16_n1.yml
+python run_exp.py -r $N -c experiments/memory/submul/l2_512_async_cpu_bt64_bf32_n1.yml
+python run_exp.py -r $N -c experiments/memory/submul/l2_512_async_cpu_bt64_bf64_n1.yml
 fi
