6e0770cd8728da00838689cbb74a95db0a3729f9
voelker.benedikt@yahoo.de <voelker.benedikt@yahoo.de>
2021-11-25 23:37:34 +0100

    new plot configs and plotting scripts

---
+++ plots/script/line_times.py
@@ -0,0 +1,9 @@
+import argparse
+from pathlib import Path
+from cliva_fl.utils import Plotter
+
+parser = argparse.ArgumentParser(description='Argument parser for summary plot creation.')
+parser.add_argument('-c', '--conf', type=Path, required=True, help='Path to the summary plot config file.')
+
+args = parser.parse_args()
+Plotter.plot_line_times(args.conf)
