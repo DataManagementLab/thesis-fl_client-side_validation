5982e8d288c9b8006892f79215f82d96f1656135
benvoe <voelker.benedikt@yahoo.de>
2021-11-25 14:39:52 +0100

    Minor improvements, non-random noise, bug bixes

--- cliva_fl/validation/method/freivald.py
+++ cliva_fl/validation/method/freivald.py
@@ -16,8 +16,8 @@ def freivald(A, B, C, bias=None, rtol=1e-05, atol=1e-08, n_check=1):
     ABr = torch.matmul(A, torch.matmul(B, R))
     Cr = torch.matmul(torch.sub(C, bias), R)
     res = tensors_close(Cr, ABr, rtol=rtol, atol=atol)
-    if not res:
-        tola = (Cr - ABr).abs()
-        tolr = ((Cr - ABr)/Cr).abs()
-        print(A.shape, B.shape, C.shape, 'atol:', tola.max().item(), 'rtol:', tolr.max().item())
+    # if not res:
+    #     tola = (Cr - ABr).abs()
+    #     tolr = ((Cr - ABr)/Cr).abs()
+    #     print(A.shape, B.shape, C.shape, 'atol:', tola.max().item(), 'rtol:', tolr.max().item())
     return res
