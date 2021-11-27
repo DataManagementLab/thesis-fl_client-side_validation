dc58239a0ae52254e7fd0b88eb2469f62e8de0e2
voelker.benedikt@yahoo.de <voelker.benedikt@yahoo.de>
2021-11-27 10:03:03 +0100

    changes for plots and exp configs

--- thesis_plots.sh
+++ thesis_plots.sh
@@ -7,9 +7,12 @@ python plots/script/freivald_scalability.py
 python plots/script/line_times.py -c plots/conf/exp_time_method_scalability.yml
 python plots/script/line_times.py -c plots/conf/exp_time_guarantee_scalability.yml
 python plots/script/timeframes.py -c plots/conf/exp_time_device_concurrency.yml
+python plots/script/line_times.py -c plots/conf/exp_time_guarantee_speed.yml
+python plots/script/times.py -c plots/conf/exp_time_gvfa_train_validate.yml
 
 # MEMORY PLOTS
 python plots/script/line_memory.py -c plots/conf/exp_mem_model_size.yml
 
 # ATTACK PLOTS
 python plots/script/line_quality.py -c plots/conf/exp_attack_varying_noise.yml
+python plots/script/line_quality.py -c plots/conf/exp_attack_guarantee_detection.yml
